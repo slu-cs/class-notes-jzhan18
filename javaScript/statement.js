0e7c0366ced2af82cc8ddecdbd80194ddd78a5d6
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-01-29 13:37:12 -0500

    JavaScript syntax

---
+++ javaScript/statement.js
@@ -0,0 +1,26 @@
+// Statement syntax in JavaScript
+// VM cs-linuxlab-20
+// constant
+const pi = 3.14;
+
+// Printing
+console.log(pi);
+
+// for loop
+for (let x = 0; x < 3; x++) {
+  console.log(x);
+}
+
+// while loop
+let x = 0;
+while (x < 3) {
+  console.log(x);
+  x +=1;
+}
+
+// conditional
+if (x === 3) {
+  console.log('x is now 3');
+} else {
+  console.log(`x is now ${x}`);
+}
