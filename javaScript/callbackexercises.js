a2c051d375de70bbdea0c110e99d7ce0d56a22a1
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-02-05 14:17:09 -0500

    callback ex1

--- javaScript/callbackexercises.js
+++ javaScript/callbackexercises.js
@@ -3,9 +3,7 @@ const numbers = [1, 2, 3, 4, 5];
 const filter = function(n, f) {
   const numbers = [];
   for (const i of n) {
-    if (f(i)) {
-      numbers.push(i);
-    }
+    numbers.push(i);
   }
 };
 const odds = filter(numbers, x => x%2 > 0);
