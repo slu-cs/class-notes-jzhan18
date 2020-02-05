00ef56173f8e1d88ba50a0800a18e1cd92048bba
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-02-05 13:44:40 -0500

    JS console input

---
+++ javaScript/input.js
@@ -0,0 +1,17 @@
+// Syntax for console and file input
+
+// synchronous calls: wait for result, stuck until it arrives
+// asynchronous calls: don't wait for result, handle the result whenever it arrivies
+
+const readline = require('readline');
+
+// Conslole configuration
+const user = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// Console input
+user.question('Enter a filename: ', function(filename) {
+  console.log(filname);
+});
