ff4621e134bce0a05d7aae505b4ccb44064c592d
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-02-12 13:16:30 -0500

    queries

--- mongo/faculty/query.js
+++ mongo/faculty/query.js
@@ -6,9 +6,26 @@ const Professor = require('./schema');
 
 connect(); // To the database
 
-// What documents are in the collection?
+/*// What documents are in the collection?
 const query = Professor.find();
 query.exec(function(error, professors) {
   if (error) console.error(error.stack);
   console.log(professors);
-});
+}); */
+
+const queries = [
+  // What are names in alphabetical order?
+  Professor.find().sort('name'),
+
+  // Who started most recently?
+  Professor.find().sort('-started').limit(1),
+
+  // Who started in 2003?
+  Professor.find().where('started').equals(2003),
+
+  // Who teaches 362?
+  Professor.find().where('courses').in(362),
+
+  // What are all the ranks?
+  Professor.distinct('rank'),
+];
