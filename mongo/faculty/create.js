ce411241a95a39e908e11cc42c481e063f18ea38
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-02-12 13:40:31 -0500

    callbacks

--- mongo/faculty/create.js
+++ mongo/faculty/create.js
@@ -29,23 +29,10 @@ const lee = new Professor({
 });
 
 // Delete any previous data
-mongoose.connection.dropDatabase(function() {
-
-  // Save the new data
-  harcourt.save(function(error) {
-    if (error) console.error(error.stack);
-
-    torrey.save(function(error) {
-      if (error) console.error(error.stack);
-
-      lee.save(function(error) {
-        if (error) console.error(error.stack);
-
-        // Disconnect
-        mongoose.connection.close(function() {
-          console.log('Database is ready.');
-        });
-      });
-    });
-  });
-});
+mongoose.connection.dropDatabase()
+  .then(() => harcourt.save())
+  .then(() => torrey.save())
+  .then(() => lee.save())
+  .then(() => mongoose.connection.close())
+  .then(() => console.log('Database is ready.'))
+  .catch(error => console.error(error.stack));
