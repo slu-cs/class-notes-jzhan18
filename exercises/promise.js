6515c1ec52297820c5ad14a1658e027aba5cd418
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-02-17 14:12:21 -0500

    Promise exercises

---
+++ exercises/promise.js
@@ -0,0 +1,34 @@
+f('hello', funciton(error, result) {
+  if (error) console.log(error.stack);
+  console.log(result);
+})
+
+// 1.
+f('hello')
+  .then(result => console.log(result))
+  .catch(error => console.error(error.stack));
+
+// 2.
+q1.exec(function(error, r1) {
+  if (error) console.log(error.stack);
+  console.log(r1);
+
+  q2.exec(function(error, r2) {
+    if (error) console.log(error.stack);
+    console.log(r2);
+  });
+});
+
+q1.exec()
+  .then(function(r1) {
+    console.log(r1);
+    return q2.exec();
+  })
+  .then(r2 => consol.log(r2))
+  .catch(error => console.log(error.stack));
+
+// 3.
+const saves = documents.map(d => d.save());
+Promise.all(saves)
+  .then(() => console.log('All saved'))
+  .catch(error => console.log(error.stack));
