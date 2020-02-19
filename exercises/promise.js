f953e0ade854e8a9485d4fa70907f12bf56104c2
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-02-19 13:18:38 -0500

    JQuery objects

--- exercises/promise.js
+++ exercises/promise.js
@@ -32,3 +32,21 @@ const saves = documents.map(d => d.save());
 Promise.all(saves)
   .then(() => console.log('All saved'))
   .catch(error => console.log(error.stack));
+
+q1.exec(function(error, r1) {
+  if (error) console.log(error.stack);
+  console.log(r1);
+
+  q2.exec(function(error, r2) {
+    if (error) console.log(error.stack);
+    console.log(r2);
+  });
+});
+
+q1.exec()
+  .then(function(r1) {
+    console.log(r1);
+    return q2.exec();
+  })
+  .then(r2 => console.log(r2));
+  .catch(error => console.log(error.stack));
