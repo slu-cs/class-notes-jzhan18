b731c81a920572a4434a626e17304415c0e0af5d
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-03-04 13:35:24 -0500

    Express routes

--- express/hello/app.js
+++ express/hello/app.js
@@ -1,12 +1,37 @@
 // Hello world web server
+// Web server setup: once per server
+// 1. npm init
+// 2. npm install express
+
 const express = require('express');
 
 // Create the server
 const app = express();
 
+// cs-linuxlab-20.stlawu.edu:3000
 // Respond to one request
 app.get('/', function(request, response) {
   response.send('Hello Jiusheng');
+});
+
+// cs-linuxlab-20.stlawu.edu:3000/foo
+app.get('/foo', function(request, response) {
+  response.send('Hello foo');
+});
+
+// cs-linuxlab-20.stlawu.edu:3000/foo/bar
+app.get('/foo/bar', function(request, response) {
+  response.send('Hello bar');
+});
+
+// cs-linuxlab-20.stlawu.edu:3000/zap/z (for any z)
+app.get('/zap/:z', function(request, response) {
+  response.send(`Hello ${request.params.z}`);
+});
+
+// cs-linuxlab-20.stlawu.edu:3000/zap?x=&y=
+app.get('/zap', function(request, response) {
+  response.send(`Hello zap with ${request.query.x} and ${request.query.y}`);
 });
 
 // Start the server
