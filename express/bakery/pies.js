b20909bfc30742ba279f655f9c97794f92eb9a1d
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-03-04 14:17:04 -0500

    Express router

---
+++ express/bakery/pies.js
@@ -0,0 +1,28 @@
+// Router for cake-related requests
+const express = require('express');
+
+// Create the router
+const router = express.Router();
+
+// cs-linuxlab-20.stlawu.edu:3000/pies/
+router.get('/', function(request, response) {
+  response.send(`
+    <h1>Cakes</h1>
+    <ul>
+      <li><a href="/pies/apple">Vanilla</a></li>
+      <li><a href="/pies/cherry">Chocolate</a></li>
+    </ul>
+    `);
+});
+
+router.get('/:id', function(request, response, next) {
+  if (request.param.id === 'apple') {
+    response.send('Apple pies');
+  } else if (reqest.param.id === 'cherry') {
+    response.send('Cherry pies');
+  } else {
+    next(); // Pass on this request
+  }
+});
+
+module.exports = router;
