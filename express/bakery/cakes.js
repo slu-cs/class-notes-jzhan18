24e62a026de85e5cd601a089b8a0b2cadad9ac56
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-03-09 14:02:53 -0400

    ejs statements

--- express/bakery/cakes.js
+++ express/bakery/cakes.js
@@ -12,13 +12,7 @@ const cakes = [
 
 // cs-linuxlab-20.stlawu.edu:3000/cakes/
 router.get('/', function(request, response) {
-  response.send(`
-    <h1>Cakes</h1>
-    <ul>
-      <li><a href="/cakes/vanilla">Vanilla</a></li>
-      <li><a href="/cakes/chocolate">Chocolate</a></li>
-    </ul>
-    `);
+  response.render('cakes/index', {cakes: cakes});
 });
 
 router.get('/:id', function(request, response, next) {
