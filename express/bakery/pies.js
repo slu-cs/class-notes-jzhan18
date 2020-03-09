f729909a7d5630bc7a0d923eafaf4fe1ce820b8a
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-03-09 14:11:31 -0400

    ejs pies

--- express/bakery/pies.js
+++ express/bakery/pies.js
@@ -4,24 +4,24 @@ const express = require('express');
 // Create the router
 const router = express.Router();
 
+// Pretend database collection
+const pies = [
+  {id: 'apple', flavor: 'Apple'},
+  {id: 'cherry', flavor: 'Cherry'}
+];
+
 // cs-linuxlab-20.stlawu.edu:3000/pies/
 router.get('/', function(request, response) {
-  response.send(`
-    <h1>Pies</h1>
-    <ul>
-      <li><a href="/pies/apple">Apple</a></li>
-      <li><a href="/pies/cherry">Cherry</a></li>
-    </ul>
-    `);
+  response.render('pies/index', {pies: pies});
 });
 
 router.get('/:id', function(request, response, next) {
-  if (request.params.id === 'apple') {
-    response.send('Apple pies');
-  } else if (request.params.id === 'cherry') {
-    response.send('Cherry pies');
+  // Pretend database lookup
+  const pie = pies.find(pie => pie.id === request.params.id);
+  if (!pie) {
+    next(); // pass on this request
   } else {
-    next(); // Pass on this request
+    response.render('pies/detail', {pie: pie});
   }
 });
 
