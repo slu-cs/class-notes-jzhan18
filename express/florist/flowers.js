78bd20517e588a963a33b68a42c15bf97a6565ac
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-03-11 14:00:00 -0400

    requests for AJAX

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -23,13 +23,42 @@ router.get('/:id', function(request, response, next) {
   }  else {
     response.send(flower);
   }
-  
 });
 
 // POST /flowers
+router.post('/', function(request, response) {
+  const flower = request.body;
+  if (!flower.id) {
+    response.status(400).send('Missing ID');
+  } else if (flower.find(f => f.id === flower.id)) {
+    response.status(400).send('Duplicate');
+  } else {
+    flowers.push(flower);
+    response.status(201).end(flowers);
+  }
+});
 
 // DELETE /flowers/id
+rouer.delete('/:id', function(requset, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next();
+  } else {
+    flowers = flowers.filter(f => f.id !== flower.id);
+    response.status(200).send(flowers);
+  }
+});
 
 // PUT /flower/id
+router.put('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next();
+  } else {
+    if (requset.body.color) flower.color = request.body.color;
+    if (requset.body.season) flower.season = requset.body.season;
+    response.status(200).send(flowers);
+  }
+});
 
 module.exports = router;
