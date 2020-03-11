11f7a209d9dd8a8f2d496b841253ef41e8a23168
Jiusheng Zhang <jzhan18@stlawu.edu>
2020-03-11 13:48:43 -0400

    REST API for AJAX

--- express/florist/flowers.js
+++ express/florist/flowers.js
@@ -3,4 +3,33 @@ const express = require('express');
 
 // Create the router
 const router = express.Router();
+
+// Pretend database
+let flowers = [
+  {id: 'tulip', color: 'pink', season: 'spring'},
+  {id: 'rose', color: 'red', season: 'summer'}
+];
+
+// GET /flowers
+router.get('/', function(request, response) {
+  response.send(flowers);
+});
+
+// GET /flowers/id
+router.get('/:id', function(request, response, next) {
+  const flower = flowers.find(f => f.id === request.params.id);
+  if (!flower) {
+    next(); // leads to 404
+  }  else {
+    response.send(flower);
+  }
+  
+});
+
+// POST /flowers
+
+// DELETE /flowers/id
+
+// PUT /flower/id
+
 module.exports = router;
